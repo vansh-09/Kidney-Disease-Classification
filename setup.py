dbadde2ad3469fa90611358ad33fc926c7711b6f
vansh-09 <vanshkjain09@gmail.com>
2025-11-14 19:17:31 +0530

    Requirements added

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 REPO_NAME = "Kidney-Disease-Classification"
 AUTHOR_USER_NAME = "vansh-09"
@@ -25,5 +25,5 @@ setuptools.setup(
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
+    packages=setuptools.find_packages(where="src")  
 )
